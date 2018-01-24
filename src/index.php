0dff42611b6395fa4c8d12733ab75f934493f8d6
corey <corey@core-industries.com>
2018-01-23 17:37:06 -0700

    hello

--- src/index.php
+++ src/index.php
@@ -19,7 +19,9 @@
                 <h2>Congratulations</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
-                <?php
+<h1>hello</h1>
+	<?php
+
                         $myfile = fopen("/var/www/my-vol/date", "r") or die("");
                         echo fread($myfile,filesize("/var/www/my-vol/date"));
                         fclose($myfile);
